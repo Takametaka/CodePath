f0235d3f0e99371a055b73e5275fb7b09555612c
Takametaka <tristan.delort@gmail.com>
2019-12-29 18:47:26 +0100

    Ex3 done in syscalls
    except for printing the output

---
+++ Niv_0_et_1/ex3.c
@@ -0,0 +1,23 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    int fd = open(argv[1],O_RDONLY);
+    if(fd == -1) return 1;
+
+    //0x20 to 0x7E
+    int freq[96] = {};
+    
+    char c;
+    while(read(fd,&c,sizeof(char)))
+        freq[(int)c - 0x20]++;
+    
+    for(int i = 0; i < 96; i++)
+        printf("%c : %d\n",(char)(i + 0x20),freq[i]);
+
+    return 0;
+}
