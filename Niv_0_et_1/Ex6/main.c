1d40d958baed7f1305fa2d1a17706f58f1530104
Takametaka <tristan.delort@gmail.com>
2019-12-30 20:40:04 +0100

    Finished ex6

--- Niv_0_et_1/Ex6/main.c
+++ Niv_0_et_1/Ex6/main.c
@@ -8,29 +8,9 @@ struct _abr {
 };
 typedef struct _abr abr_t;
 
-//return 1 -> wrong number of arguments
-//return 2 -> failed to read input file
-//return 3 -> failed to allocate tree node
-int main(int argc, char **argv)
-{   
-    if(argc != 2) exit(1);
-    FILE *fstr = fopen(argv[1],"r");
-    if(fstr == NULL) exit(2);
-    
-    int input;
-   abr_t *root = NULL; 
-    while(fscanf(fstr,"%d\n",&input) != EOF)
-    {
-        root = pushAbr(root,input);
-        //continue here
-    }
-    fclose(fstr);
-    return 0;
-}
-
 abr_t *newAbr(int data)
 {
-    abr_t *Node = (abt_t *)malloc(sizeof(struct _abr));
+    abr_t *Node = (abr_t *)malloc(sizeof(struct _abr));
     if(Node == NULL) exit(3);
     Node->data = data;
     Node->d = NULL;
@@ -43,9 +23,73 @@ abr_t *pushAbr(abr_t *root,int data)
 {
     if(root == NULL)
         return newAbr(data);
-    else if(root->data >= data)
+    
+    //evite les doublons dans l'arbre
+    if(data == root->data)
+        return root;
+
+    if(root->data >= data)
         root->d = pushAbr(root->d,data);
     else if(root->data < data)
         root->g = pushAbr(root->g,data);
     return root;
+}
+
+void destroyAbr(abr_t *root)
+{
+    if(root == NULL) return;
+    abr_t *g = root->g;
+    abr_t *d = root->d;
+    free(root);
+    destroyAbr(g);
+    destroyAbr(d);
+}
+
+void scanAbr(abr_t *root,FILE *dot)
+{
+    if(root == NULL) return;
+    if(root->d != NULL) 
+    {
+        if(root->data != root->d->data)
+        {
+            fprintf(dot,"%d--%d\n",root->data,root->d->data);
+            scanAbr(root->d,dot);
+        }
+    }
+    if(root->g != NULL) 
+    {
+        if(root->data != root->g->data)
+        {
+            fprintf(dot,"%d--%d\n",root->data,root->g->data);
+            scanAbr(root->g,dot);
+        }
+    }
+}
+
+//return 1 -> wrong number of arguments
+//return 2 -> failed to open file
+//return 3 -> failed to allocate tree node
+int main(int argc, char **argv)
+{   
+    if(argc != 2) exit(1);
+    FILE *fstr = fopen(argv[1],"r");
+    if(fstr == NULL) exit(2);
+    
+    FILE *dot = fopen("tree.dot","w");
+    if(dot == NULL) exit(2);
+
+    int input;
+    abr_t *root = NULL; 
+    //make tree
+    while(fscanf(fstr,"%d\n",&input) != EOF)
+        root = pushAbr(root,input);
+    //scan tree
+    fprintf(dot,"graph tree {\n");
+    scanAbr(root,dot);
+    fprintf(dot,"}\n");
+
+    destroyAbr(root);
+    fclose(dot);
+    fclose(fstr);
+    return 0;
 }
--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 # CodePath
 
-Exercices of the ZenCodePath.pdf written by yaspr
+Exercices du ZenCodePath.pdf ecrit par yaspr
 
+# Niv\_0\_et\_1
+
+Ex0 : *Somme des elements d'un tableau*  
+
+Ex1 : *Somme des elements pair/impair d'un tableau*  
+Note : 2 implémentation pour voir laquelle est la plus optimisée  
+
+Ex2 : *Plus long mot (et sa taille) d'une chaine de char*  
+
+Ex3 : *frequences des char ascii d'un fichier*  
+
+Ex4 : *Entier premier ?*  
+
+Ex5 : *Estimation de phi avec la suite de fibonacci*  
+
+Ex6 : *Arbre binaire de recherche a partir d'entier d'un fichier*  
